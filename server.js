d60d136809cce3a8f7386044448a7097ee542991
fmarivas <felixviage@gmail.com>
2024-03-16 19:28:23 +0200

    adicao de porta via env

--- server.js
+++ server.js
@@ -17,7 +17,7 @@ const session = require('express-session')
 const passport = require('passport')
 
 const app = express()
-const port = 3000
+const port = process.env.PORT ||  3000
 
 
 app.use(bodyParser.urlencoded({ extended: true }));
